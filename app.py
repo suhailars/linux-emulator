336fa4e0858e4da8bdeea6564feea5cf948c4643
suhail <suhail>
2016-10-28 16:21:26 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,59 @@
+import os
+from flask import Flask, render_template, request, json, url_for, redirect, session
+import urllib
+from subprocess import Popen, PIPE, STDOUT
+from flask.ext.pymongo import PyMongo
+
+SKIP = [
+  "sudo",
+  "rm",
+  "compgen",
+  "gnome-sudoku"
+]
+
+app = Flask(__name__)
+app.config['MONGO_DBNAME'] = 'restdb'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/restdb'
+mongo = PyMongo(app)
+
+
+@app.route('/')
+def hello():
+    return redirect(url_for('index'))
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/onload', methods=['GET'])
+def translator():
+    shell_command = 'bash command.sh'
+    event = Popen(shell_command, shell=True, stdin=PIPE, stdout=PIPE,
+        stderr=STDOUT)
+
+    output = event.communicate()
+    resp = output[0].split('\n')
+    resp = sorted(list(set(resp)))
+
+    return json.dumps({'status':'OK', 'resp':resp})
+
+@app.route('/getresult', methods=['POST'])
+def getReslut():
+    data = request.json
+    data = str(data[u'data'])
+    item = {"cmd": data}
+    cmd = data.split()[0]
+    if cmd in SKIP:
+       output = "not perimited!!!"
+    else:
+        event = Popen(data, shell=True, stdin=PIPE, stdout=PIPE,
+            stderr=STDOUT)
+        output = event.communicate()
+        output = output[0]
+    history = mongo.db.history
+    history.insert(item)
+    print [i for i in mongo.db.history.find()]
+    return json.dumps({'status':'OK', 'resp':output})
+
+if __name__=="__main__":
+    app.run(debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,75 @@
+$(function(){
+  var commands,
+    getResult = function(val) {
+      console.log("at val #####", val);
+      var val = {"data": val};
+
+      $.ajax({
+        url: '/getresult',
+        data: val,
+        type: 'POST',
+        data: JSON.stringify(val),
+        contentType: 'application/json;charset=UTF-8',
+        success: function(response){
+          var result = JSON.parse(response).resp;
+
+          console.log(JSON.parse(response).resp);
+          $('#result').val(result);
+        },
+        error: function(error){
+        	console.log(error);
+        }
+      });
+    };
+
+  $( window ).load(function() {
+    // Run code
+    console.log("loaded")
+    $.ajax({
+      url: '/onload',
+      type: 'GET',
+      success: function(response){
+      	commands = JSON.parse(response).resp;
+        console.log(commands);
+        $('#english')
+        .on('keydown', function(event){
+          //console.log("event ****", event, typeof($( this ).autocomplete));
+          if ( event.keyCode === $.ui.keyCode.TAB &&
+            $( this ).autocomplete( "instance" ).menu.active ) {
+          //console.log("event ****", event);
+              event.preventDefault();
+          } else if (event.keyCode === $.ui.keyCode.ENTER) {
+            var val = $('#english').val();
+
+            getResult(val);
+          }
+        })
+        .autocomplete({
+          minLength:2,
+          delay:500,
+          source: function(request, response) {
+            var results = $.ui.autocomplete.filter(commands, request.term);
+
+            response(results.slice(0, 10));
+          }
+          //select: function( event, ui ) {
+          //  var terms = this.value;
+          //  console.log("at select *****", terms);
+          //}
+        });
+      },
+      error: function(error){
+      	console.log(error);
+      }
+    });
+  });
+  console.log("commands", commands);
+
+  $('#').on('keydown', (function(event){
+    var user = $('#english').val();
+    var pass = $('#other').val();
+    console.log(event);
+    return;
+
+  }));
+});
